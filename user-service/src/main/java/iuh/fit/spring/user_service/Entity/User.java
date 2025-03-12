3053ba24aa9daa830625624aaff384d6aa8e4abc
KhangNinh123 <ninhtrinh744@gmail.com>
2025-03-12 14:52:05 +0700

    add user-service

---
+++ user-service/src/main/java/iuh/fit/spring/user_service/Entity/User.java
@@ -0,0 +1,81 @@
+package iuh.fit.spring.user_service.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String role; // USER, ADMIN
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User() {
+
+    }
+
+    public User(String password, Long id, String username, String email, String role) {
+        this.password = password;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ user-service/src/main/java/iuh/fit/spring/user_service/Service/UserService.java
@@ -0,0 +1,31 @@
+package iuh.fit.spring.user_service.Service;
+import iuh.fit.spring.user_service.Entity.User;
+import iuh.fit.spring.user_service.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(Long id, User user) {
+        user.setId(id);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
